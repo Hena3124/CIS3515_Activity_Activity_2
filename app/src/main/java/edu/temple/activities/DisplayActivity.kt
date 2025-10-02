2c64b157f7a4eb8ca32c1aae271b7197b94df1df
Hena Patel <hena.patel0002@temple.edu>
2025-10-02 15:14:37 -0400

    First Updates

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -4,8 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContracts
 
+const val KEY = "SIZE"
 class DisplayActivity : AppCompatActivity() {
+
+
+     val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == RESULT_OK){
+            it.data?.apply {
+                lyricsDisplayTextView.textSize = getIntExtra(KEY,12).toFloat()
+            }
+        }
+    }
 
     // TODO Step 1: Launch TextSizeActivity when button clicked to allow selection of text size value
 
@@ -20,6 +32,11 @@ class DisplayActivity : AppCompatActivity() {
 
         lyricsDisplayTextView = findViewById(R.id.lyricsDisplayTextView)
         textSizeSelectorButton = findViewById(R.id.textSizeSelectorButton)
+
+        textSizeSelectorButton.setOnClickListener {
+            val intent = Intent(this@DisplayActivity, TextSizeActivity::class.java)
+            launcher.launch(intent)
+        }
 
     }
 }
--- app/src/main/java/edu/temple/activities/TextSizeActivity.kt
+++ app/src/main/java/edu/temple/activities/TextSizeActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -24,12 +25,13 @@ class TextSizeActivity : AppCompatActivity() {
 
             // TODO Step 2: Pass selected value back to activity that launched TextSizeActivity
             adapter = TextSizeAdapter(textSizes){
-
+                val intent = Intent()
+                intent.putExtra(KEY, it)
+                setResult(RESULT_OK, intent)
+                finish()
             }
             layoutManager = LinearLayoutManager(this@TextSizeActivity)
         }
-
-
 
     }
 }
